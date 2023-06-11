d6e8c6d7026b6c0be13b64e903329cba935cd1fd
volkmar Rigo <volkmar@rigo.bz>
2023-06-12 00:08:17 +0200

    Moved from Config file to Config in Code

---
+++ src/ReverseProxy/Options/ReverseProxyOptions.cs
@@ -0,0 +1,37 @@
+﻿using Yarp.ReverseProxy.Configuration;
+
+namespace ReverseProxy;
+
+public class ReverseProxyOptions
+{
+    private const string QuestionAppCluster = nameof(QuestionAppCluster);
+    private const string QuestionAppRoute = nameof(QuestionAppRoute);
+
+    public string QuestionsAppUrl { get; set; } = "";
+
+    public ClusterConfig[] GetClusters()
+        => new[]
+        {
+            new ClusterConfig
+            {
+                ClusterId = QuestionAppCluster,
+                Destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase)
+                {
+                    {"Site1", new DestinationConfig() { Address = QuestionsAppUrl } }
+                }
+            }
+        };
+
+    public RouteConfig[] GetRoutes()
+     => new[]
+     {
+         new RouteConfig {
+                ClusterId = QuestionAppCluster,
+                RouteId = QuestionAppRoute,
+                Match = new()
+                {
+                    Path = "{**catch-all}",
+                }
+         }
+     };
+}
--- src/ReverseProxy/Program.cs
+++ src/ReverseProxy/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Observability;
+using ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,8 +8,13 @@ var observabilityOptions = new ObservabilityOptions();
 builder.Configuration.Bind("Observability", observabilityOptions);
 builder.Services.AddObservability("Rigo.ReverseProxy", typeof(Program), observabilityOptions);
 
+var reverseProxyOptions = new ReverseProxyOptions();
+builder.Configuration.Bind("ReverseProxy", reverseProxyOptions);
 
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddReverseProxy().LoadFromMemory(reverseProxyOptions.GetRoutes(), reverseProxyOptions.GetClusters());
 
 var app = builder.Build();
 
